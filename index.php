0e367bb9f09d0c9276a5682920443a9cb73e36e4
FinnT730 <finnt730@gmail.com>
2022-05-29 23:18:05 +0200

    Sort of in a working state, INIT commit has it in a fully working state, but it is not using the Teams array, and is only hardcoded to 2 teams instead, which is not something I wish to have.

--- index.php
+++ index.php
@@ -7,33 +7,76 @@ include "_match.php";
 global $matches;
 $matches = [];
 
+$teams = [
+    ["name" => "Team A", "players" => 10],
+    ["name" => "Team B", "players" => 10],
+    ["name" => "Team C", "players" => 10],
+    ["name" => "Team D", "players" => 10],
+];
 
-function createMatch(int $id, $_matchArr, _match $lastm = null) {
-    $t1 = new Team("team 1",10);
-    $t2 = new Team("team " . random_int(0,10),10);
-    $a = null;
-    if(is_null($lastm)) {
-        $a = new _match($t1,$t2);
-    } else {
-        $a = new _match($t1,$t2,$lastm);
+echo '<pre>' , print_r(var_dump($teams)) , '</pre>';
+
+
+function createMatch(int $id, $_matchArr, $_teams , _match $lastm = null) {
+//    $t1 = new Team("team 1",10);
+//    $t2 = new Team("team " . random_int(0,10),10);
+
+    for($i = 0; $i < count($_teams); $i += 2) {
+        $a = null;
+        $t1 = new Team($_teams[$i][0]->name, $_teams[$i][1]->players);
+        $t2 = new Team($_teams[$i+1][0]->name, $_teams[$i+1][1]->players);
+        if(is_null($lastm)) {
+            $a = new _match($t1,$t2);
+        } else {
+            $a = new _match($t1,$t2,$lastm);
+        }
+        $_matchArr[$id] = $a;
+        $id += 1;
     }
+
+
+//
+//    $a = null;
+//    if(is_null($lastm)) {
+//        $a = new _match($t1,$t2);
+//    } else {
+//        $a = new _match($t1,$t2,$lastm);
+//    }
 //    $a = new _match($t1,$t2);
-    $_matchArr[$id] = $a;
+//    $_matchArr[$id] = $a;
 
     return $_matchArr;
 }
 
 
-$matches = createMatch(0,$matches);
-$matches = createMatch(1,$matches,$matches[0]);
-$matches = createMatch(2,$matches,$matches[1]);
-$matches = createMatch(3,$matches,$matches[2]);
+$matches = createMatch(0,$teams,$matches);
+//$matches = createMatch(1,$teams,$matches,$matches[0]);
+//$matches = createMatch(2,$teams,$matches,$matches[1]);
+//$matches = createMatch(3,$teams,$matches,$matches[2]);
 
 
 //echo '<pre>' , print_r(var_dump($matches[1])) , '</pre>';
 
 
 foreach ($matches as $a => $value) {
-    echo "Match : " . $value->id;
-    echo("<br>" . $value->team2 . '<br>');
+
+    echo "{{{{ <br>";
+    foreach ($value as $qq => $qv) {
+        echo $qq . "<br>";
+    }
+    echo "}}}} <br>";
+
+    echo "{ ";
+
+//    echo "Match : " . $value->id;
+//    echo("<br>" . $value . '<br>');
+
+//    if($value->lastMatch != null) {
+//        foreach ($value as $b => $vb) {
+//            echo("Match [ " . $vb . " ] <br>");
+////            echo("[ LAST MATCH(S) = " . $vb . " ] <br>");
+//        }
+//    }
+    echo " }";
+    echo '<br>';
 }
